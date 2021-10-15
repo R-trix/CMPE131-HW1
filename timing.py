ec97ebd99a44eefb2027ce02781c3ff50e4a12da
Brendan Requierme <requiermeb@gmail.com>
2021-10-14 22:00:38 -0700

    Reverted changes to timing.py, keeping X as an int.

--- timing.py
+++ timing.py
@@ -2,11 +2,9 @@ import time
 
 def calculate_time(func):
     start_time = time.time()
-    print(start_time)
     func()
     end_time = time.time()
-    print(end_time)
     total_time = end_time - start_time
     X = int(total_time)
-    s = 'Total time is ' + X
-    return str(s)
+    print ('Total time ' + X)
+
