bada6aa3eda9c4e4fa3ee96ffbfa806a1510d869
Brendan Requierme <requiermeb@gmail.com>
2021-10-14 21:55:48 -0700

    Another change in the timing.py function. Now returning str(s).

--- timing.py
+++ timing.py
@@ -2,9 +2,11 @@ import time
 
 def calculate_time(func):
     start_time = time.time()
+    print(start_time)
     func()
     end_time = time.time()
+    print(end_time)
     total_time = end_time - start_time
     X = int(total_time)
-    return ('Total time %s' %(X))
-
+    s = 'Total time is ' + X
+    return str(s)
