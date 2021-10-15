5464e3f9daf9eb82e795ef21a3ed7101b14bc856
Brendan Requierme <requiermeb@gmail.com>
2021-10-14 22:19:15 -0700

    Modified print statement to align with wanted output.

--- timing.py
+++ timing.py
@@ -1,10 +1,18 @@
 import time
 
 def calculate_time(func):
-    start_time = time.time()
-    func()
-    end_time = time.time()
-    total_time = end_time - start_time
-    X = int(total_time)
-    print ('Total time ' + X)
+    '''
+    Decorator function to calculate time it takes to run a function.
+    '''
+    def wrapper():
+        '''
+        Inner function to operate the decorator.
+        '''
+        start_time = time.time()
+        func()
+        end_time = time.time()
+        total_time = end_time - start_time
+        print (f'Total time {total_time}')
+    return wrapper()
+    
 
