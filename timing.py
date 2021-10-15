8a64754829fab55429fdce63f366c9c31180efc8
Brendan Requierme <requiermeb@gmail.com>
2021-10-14 21:30:58 -0700

    Modified timing.py to output string as assignment says.

--- timing.py
+++ timing.py
@@ -4,8 +4,7 @@ def calculate_time(func):
     start_time = time.time()
     func()
     end_time = time.time()
-    print("Total time " + (start_time - end_time))
-    
-
-time.sleep(2)
+    total_time = start_time - end_time
+    X = str(int(total_time))
+    print ('Total time ' + X)
 
