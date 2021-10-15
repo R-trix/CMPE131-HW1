f1c2bb50076b775c96cf9f10928deaf78c80d4d2
Brendan Requierme <requiermeb@gmail.com>
2021-10-14 21:37:05 -0700

    Rewrote the total_time variable because I calculated it wrong...

--- timing.py
+++ timing.py
@@ -4,7 +4,7 @@ def calculate_time(func):
     start_time = time.time()
     func()
     end_time = time.time()
-    total_time = start_time - end_time
-    X = str(int(total_time))
+    total_time = end_time - start_time
+    X = int(total_time)
     print ('Total time ' + X)
 
